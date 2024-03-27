0eda4aa2555ebc5f248de4df04ed995df78501f9
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-03-27 12:50:42 +0100

    Add main part: resources containing CP1250 definition and version info (both internal and for Windows)

---
+++ resources.h
@@ -0,0 +1,13 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+#include <windows.h>
+
+#define IDS_CPNAME      1
+#define IDS_USEDWORD    2
+#define IDS_REVDATE     3
+#define IDS_AUTHOR      4
+
+#define IDR_CODEPAGE    100
+
+#endif
