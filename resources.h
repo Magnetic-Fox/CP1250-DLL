1a953389efd0d5ab53241a3e545d30a877c126aa
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-03-27 15:29:10 +0100

    Update resources to add library type

--- resources.h
+++ resources.h
@@ -3,10 +3,11 @@
 
 #include <windows.h>
 
-#define IDS_CPNAME      1
-#define IDS_USEDWORD    2
-#define IDS_REVDATE     3
-#define IDS_AUTHOR      4
+#define IDS_LIBTYPE     1
+#define IDS_CPNAME      2
+#define IDS_USEDWORD    3
+#define IDS_REVDATE     4
+#define IDS_AUTHOR      5
 
 #define IDR_CODEPAGE    100
 
